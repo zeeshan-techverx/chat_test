936fa78ea5a98854552c463870d9932d215bec16
zeeshan-techverx <xeeshan352@gmail.com>
2017-11-03 12:25:33 +0500

    added "inverse_of" in associations

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,7 +2,7 @@ class ChatRoom < ApplicationRecord
   
   scope :by_user, -> (id) { where('user_id = ? OR receiver_id = ?', id, id) }
 
-  belongs_to :user
+  belongs_to :user, inverse_of: :chat_rooms
   has_many :messages, dependent: :destroy
 
   validates :title, presence: true
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :user
-  belongs_to :chat_room
+  belongs_to :user, inverse_of: :messages
+  belongs_to :chat_room, inverse_of: :messages
   
   validates :body, presence: true, length: {minimum: 2, maximum: 1000}
   after_create_commit { MessageBroadcastJob.perform_later(self) }
